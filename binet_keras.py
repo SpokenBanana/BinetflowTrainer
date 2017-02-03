8f0b301a4fd24637f4441dd1f67facf2e8a158d3
David Santana <santanavidda@gmail.com>
2017-02-03 00:56:55 -0500

    separated test and train function

--- binet_keras.py
+++ binet_keras.py
@@ -1,24 +1,9 @@
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
+from keras.layers import Dense, Dropout
 from sklearn.model_selection import train_test_split
 
 
 class Binet_Keras():
-
-    def __init__(self):
-        self.model = Sequential()
-
-    def train_and_test(self, feat_train, label_train,
-                       feat_test=None, label_test=None):
-        if feat_test is None or label_test is None:
-            feat_train, feat_test, label_train, label_test = train_test_split(
-                                feat_train, label_train, test_size=0.5,
-                                random_state=42)
-        self.train(feat_train, label_train)
-        loss_and_metrics = self.test(feat_test, label_test)
-
-        # Accuracy, precision, recall
-        return loss_and_metrics[1:]
 
     def train(self, feat_train, label_train):
         self.model = Sequential()
@@ -37,3 +22,17 @@ class Binet_Keras():
     def test(self, feat_test, label_test):
         return self.model.evaluate(feat_test, label_test,
                                    batch_size=32, verbose=False)
+
+
+def keras_train_and_test(feat_train, label_train,
+                         feat_test=None, label_test=None):
+    if feat_test is None or label_test is None:
+        feat_train, feat_test, label_train, label_test = train_test_split(
+                            feat_train, label_train, test_size=0.5,
+                            random_state=42)
+    ke = Binet_Keras()
+    ke.train(feat_train, label_train)
+    loss_and_metrics = ke.test(feat_test, label_test)
+
+    # Accuracy, precision, recall
+    return loss_and_metrics[1:]
