18dc401890ee2e6b9329f6825864951ad09bc6eb
David Santana <santanavidda@gmail.com>
2017-02-24 08:54:13 -0500

    Added more metrics to computer

--- stats.py
+++ stats.py
@@ -1,14 +1,18 @@
 from main import train_and_test_step, aggregate_and_pickle, \
-        train_and_test_with, train_with_tensorflow
+        train_and_test_with, train_with_tensorflow, test_and_train_bots
 from utils import get_feature_labels, get_binetflow_files, get_start_time_for,\
         get_saved_data, mask_features, get_classifier
 from summarizer import Summarizer
+from sklearn.metrics import precision_recall_curve, auc
+from sklearn.cross_validation import ShuffleSplit
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
 import pytablewriter
 from joblib import Parallel, delayed
 from sklearn import metrics
-from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import cross_val_score, KFold, train_test_split
 from binet_keras import keras_train_and_test
 import numpy as np
+import pickle
 
 
 windows = [.15]  # , 1, 2, 5]
@@ -16,9 +20,9 @@ binet_files = get_binetflow_files()
 
 
 def run_files(f, window):
-    if get_saved_data(window, f) is None:
-        print('aggregating {} for {}s'.format(f, window))
-        aggregate_and_pickle(window, f)
+    # if get_saved_data(window, f) is None:
+    print('aggregating {} for {}s'.format(f, window))
+    aggregate_and_pickle(window, f)
 
 
 def window_shift(window):
@@ -28,14 +32,19 @@ def window_shift(window):
     value_matrix = []
     for file_name in binet_files:
         values = []
+        print(file_name)
         feature, label = get_feature_labels(
                 get_saved_data(window, file_name))
+        feature = mask_features(feature)
+        feature = feature[:int(len(feature) * .05)]
+        label = label[:int(len(label) * .05)]
         values += [file_name, '{0:.4f}'.format(
-                        train_and_test_step(feature, label, 'dt', 500)),
+                        train_and_test_step(feature, label, 'dt', 1000)),
                    '{0:.4f}'.format(train_and_test_step(
-                        feature, label, 'rf', 500))]
+                        feature, label, 'rf', 1000))]
         values.append('{0:.4f}\n'.format(
-            train_and_test_step(feature, label, 'tf', 500)))
+            train_and_test_step(feature, label, 'tf', 1000)))
+        print(values)
         value_matrix.append(values)
 
     writer.value_matrix = value_matrix
@@ -43,32 +52,36 @@ def window_shift(window):
 
 
 def file_stats():
-    mls = ['dt', 'rf']
+    mls = ['dt', 'rf']  # , 'svm', 'nb']
     for window in windows:
         writer = pytablewriter.MarkdownTableWriter()
         writer.table_name = 'File Accuracy for {}s'.format(window)
-        writer.header_list = ['File', 'Descision Tree', 'Random Forest',
+        writer.header_list = ['File', 'Decision Tree', 'Random Forest',
                                                         'Tensorflow']
         value_matrix = []
         for name in binet_files:
             values = [name]
             feature, label = get_feature_labels(get_saved_data(window, name))
             feature = mask_features(feature)
+            feat_train, feat_test, label_train, label_test = train_test_split(
+                feature, label, test_size=0.3, random_state=42)
+            z = 0
+            o = 0
             for ml in mls:
-                r = train_and_test_with(feature, label, ml)
+                r = train_and_test_with(feat_train, label_train, ml, feat_test,
+                                        label_test)
                 values.append('{0:.4f}, {1:.4f}, {2:.4f}'.format(
                               r['accuracy'],
                               r['precision'],
                               r['recall']))
-                clf = get_classifier(ml)
-                values.append('{}'.format(cross_val_score(clf, feature, label,
-                                                          cv=5,
-                                                          scoring='f1_macro')))
-                correctness, precision, recall = \
-                    keras_train_and_test(feature, label, dimension=12)
-                values.append('{0:.4f}, {1:.4f}, {2:.4f}\n'.format(correctness,
-                                                                   precision,
-                                                                   recall))
+                print(values)
+            correctness, precision, recall = \
+                keras_train_and_test(feat_train, label_train,
+                                     feat_test, label_test, dimension=17)
+            values.append('{0:.4f}, {1:.4f}, {2:.4f}'.format(correctness,
+                                                               precision,
+                                                               recall))
+            print(values)
             value_matrix.append(values)
 
         writer.value_matrix = value_matrix
@@ -86,12 +99,32 @@ def kfold_test():
         for name in binet_files:
             values = [name]
             feature, label = get_feature_labels(get_saved_data(window, name))
-            feature = mask_features(feature)
+            feature = feature[:int(len(feature) * 10)]
+            label = feature[:int(len(label) * 10)]
+            kf = KFold(n_splits=10)
+
+            # feature = mask_features(feature)
             for ml in mls:
-                clf = get_classifier(ml)
-                scores = cross_val_score(clf, feature, label, cv=10, n_jobs=2)
-                values.append('{0:.4f}, {1:.4f}'.format(np.mean(scores),
-                                                        np.std(scores)))
+                scores = []
+                pr_scores = []
+                for train, test in kf.split(feature):
+                    clf = get_classifier(ml)
+                    xtrain, ytrain = feature[train], label[train]
+                    xtest, ytest = feature[test], label[test]
+                    # scores = cross_val_score(clf, feature, label, cv=10, n_jobs=2)
+                    clf.fit(xtrain, ytrain)
+                    test_predicts = clf.predict(xtest)
+                    test_score = accuracy_score(ytest, test_predicts)
+
+                    scores.append(test_score)
+                    proba = clf.predict_proba(xtest)
+
+                    precision, recall, pr_thresholds = precision_recall_curve(
+                            ytest, proba[:, 1])
+                    pr_scores.append(auc(recall, precision))
+                values.append('{0:.4f}, {1:.4f}, {2:.4f}, {3:.4f}'.format(
+                                    np.mean(scores), np.std(scores),
+                                    np.mean(pr_scores), np.std(pr_scores)))
             kf = KFold(n_splits=10)
             accuracy = []  # , precision, recall = [], [], []
             for train_index, test_index in kf.split(feature):
@@ -112,46 +145,80 @@ def kfold_test():
 
 def bots_test():
     mls = ['dt', 'rf']
+    order = ['n_dports>1024', 'background_flow_count', 'n_s_a_p_address', 'avg_duration', 'n_s_b_p_address',
+             'n_sports<1024', 'n_sports>1024', 'n_conn', 'n_s_na_p_address', 'n_udp', 'n_icmp', 'n_d_na_p_address',
+             'n_d_a_p_address', 'n_s_c_p_address', 'n_d_c_p_address', 'normal_flow_count', 'n_dports<1024',
+             'n_d_b_p_address', 'n_tcp']
+    bots = {
+        'Neris': [1, 2, 9],
+        'Rbot': [3, 4, 10, 11],
+        'Virut': [5, 13],
+        'Menti': [6],
+        'Sogou': [7],
+        'Murlo': [8],
+        'NSIS.ay': [12]
+    }
+    bot = ['Neris', 'Rbot', 'Virut', 'Menti', 'Sogou', 'Murlo', 'NSIS.ay']
+    bot_data = [0 for _ in binet_files]
+    for key, value in bots.items():
+        for v in value:
+            bot_data[v-1] = bot.index(key)
     for window in windows:
         writer = pytablewriter.MarkdownTableWriter()
         writer.table_name = 'Bot accuracy for {}s'.format(window)
         writer.header_list = ['Bot', 'Descision Tree', 'Random Forest',
                               'Tensorflow']
-        bots = {
-                'Neris': [1, 2, 9],
-                'Rbot': [3, 4, 10, 11],
-                'Virut': [5, 13],
-                'Menti': [6],
-                'Sogou': [7],
-                'Murlo': [8],
-                'NSIS.ay': [12]
-        }
         value_matrix = []
+        features = []
+        labels = []
+        """
+        for i, b in enumerate(binet_files):
+            summary = get_saved_data(window, b)
+            for s in summary:
+                if s.is_attack:
+                    classes = [0 for _ in bot]
+                    classes[bot_data[i]] = 1
+                    labels.append(classes)
+                    features.append([s.data[o] for o in order])
+        features = np.array(features)
+        labels = np.array(labels)
+        """
+         with open('bot_features.pk1', 'rb') as f:
+            features = pickle.load(f)
+        with open('bot_labels.pk1', 'rb') as f:
+            labels = pickle.load(f)
 
-        for bot, num in bots.items():
-            summaries = []
-            values = [bot]
-
-            for s in num:
-                summaries += get_saved_data(window, binet_files[s-1])
-            feature, label = get_feature_labels(summaries)
-            for ml in mls:
-                r = train_and_test_with(feature, label, ml)
-                values.append('{0:.4f}, {1:.4f}, {2:.4f}'.format(r['accuracy'],
-                              r['precision'], r['recall']))
-            correctness, precision, recall = keras_train_and_test(feature,
-                                                                  label)
-            values.append('{0:.4f}, {1:.4f}, {2:.4f}\n'.format(correctness,
-                           precision, recall))
-            value_matrix.append(values)
+        values = [bot]
+        for ml in mls:
+            r = test_and_train_bots(features, labels, ml)
+            values.append('{0:.4f}, {1:.4f}, {2:.4f}'.format(r['accuracy'],
+                          r['precision'], r['recall']))
+        correctness, precision, recall = keras_train_and_test(features,
+                                                              labels,
+                                                              out=7)
+        values.append('{0:.4f}, {1:.4f}, {2:.4f}\n'.format(correctness,
+                       precision, recall))
+        value_matrix.append(values)
         writer.value_matrix = value_matrix
         writer.write_table()
 
 
+def stats_on_best():
+    best = [1, 3, 9, 12]
+    summaries = []
+    for b in best:
+        summaries += get_saved_data(0.15, binet_files[b])
+    feature, label = get_feature_labels(summaries)
+
+
+
+
 # Parallel(n_jobs=2)(delayed(run_files)(name, 0.15) for name in binet_files)
 # Parallel(n_jobs=2)(delayed(window_shift)(i) for i in windows)
+# window_shift(0.15)
 # print('For tuned down features of size 12')
+# print("70/30 split")
 # file_stats()
-# bots_test()
+bots_test()
 # window_shift(.15)
-kfold_test()
+# kfold_test()
